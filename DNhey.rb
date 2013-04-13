d935dd4f4acaf4e18cc189b52c3f50ddefdd41f8
Michael Nicoll <michaelnicoll@gmail.com>
2013-04-13 22:46:54 +0100

    Can now input any text and create arrays

--- DNhey.rb
+++ DNhey.rb
@@ -7,12 +7,51 @@ def read_fasta_to_array file_name
 
 	codons = []
   while (line = file.gets)
-    codons.push(line[0,3])
+    codons.push(line_to_codons line)
   end
 
   file.close
 
-  return codons
+  return codons.flatten
+end
+
+def line_to_codons line
+	index = 0
+	codons = []
+	while((codon = line[index,3]).length == 3)
+		codons.push(codon)
+		index = index + 3
+	end
+
+	codons
+end
+
+describe '.line_to_codons' do
+	describe 'when given a string containing 3 codons' do
+		before do
+			@result = line_to_codons("CTGCTGATG")
+		end
+		it 'returns an array' do
+			assert_equal(Array, @result.class)
+		end
+		it 'returns a array containing 3 elements' do
+			assert_equal 3, @result.size
+		end
+		it 'returns the correct elements' do
+			assert_equal ['CTG','CTG','ATG'], @result
+		end
+	end
+	describe 'when given a string containing 2 codons' do
+		before do
+			@result = line_to_codons("TGCTGA")
+		end
+		it 'returns an array containing 2 elements' do
+			assert_equal 2, @result.size
+		end
+		it 'returns the correct elements' do
+			assert_equal ['TGC','TGA'], @result
+		end
+	end
 end
 
 describe '.read_fasta_to_array' do
@@ -37,6 +76,9 @@ describe '.read_fasta_to_array' do
 	describe 'given the filename of a file with 5 codons' do
 		before do
 			@result = read_fasta_to_array('test_assets/5codons.fasta')
+		end
+		it 'returns an array with 5 elements' do
+			assert_equal 5, @result.size
 		end
 		it 'returns the correct first element (CTG)' do
 			assert_equal 'CTG', @result[0]
