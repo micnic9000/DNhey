3745f2f030147878fdaadfc3bd65cf9cf2521c14
Michael Nicoll <michaelnicoll@gmail.com>
2013-04-13 19:20:39 +0100

    Display error on invalid file

--- DNhey.rb
+++ DNhey.rb
@@ -1,12 +1,29 @@
 require 'minitest/autorun'
 
-def read_fasta_to_array
-  []
+def read_fasta_to_array file_name
+	file = File.new(file_name, "r")
+  [0,0]
 end
 
+
 describe '.read_fasta_to_array' do
-	it 'returns an array' do
-		result = read_fasta_to_array()
-		assert_equal Array, result.class
+	describe 'given the filename of a file with 2 codons' do
+		before do
+			@result = read_fasta_to_array('test_assets/2codons.fasta')
+		end
+		it 'returns an array' do
+			assert_equal Array, @result.class
+		end
+		it 'returns an array with 2 elements' do
+			assert_equal 2, @result.size
+		end
+	end
+
+	describe 'given an invalid filename' do
+    it 'throws an error' do
+      assert_raises(Errno::ENOENT) do
+      	read_fasta_to_array('fdhjkafdshjksadfkjhadsf')
+      end
+    end
 	end
 end
